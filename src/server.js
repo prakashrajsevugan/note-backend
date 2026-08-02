ec51105ba70af1f6b61f834504b3c419233c5b5f
prakashrajsevugan <prakashraj8739@gmail.com>
2026-08-02 21:55:50 +0530

    Changes in cron

--- src/server.js
+++ src/server.js
@@ -1,14 +1,9 @@
 require("dotenv").config();
 
 const app = require("./app");
-const healthCron = require("./jobs/healthCron");
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
-
-  // Start the cron job
-  healthCron.start();
-  console.log("✅ Health cron job started.");
 });
